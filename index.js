a202bb67792f14c9d83d6809567099a2c9c322e6
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-08-08 11:34:42 +0800

    taek

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const bodyParser = require("body-parser")
 dotenv.config();
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json());
 app.use(cors())  
 app.use(router)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
