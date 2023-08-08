590057029d591ffc75fbf4def365abd75b52e30c
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-08-08 11:08:59 +0800

    true

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(router)
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Backend server is running!');
